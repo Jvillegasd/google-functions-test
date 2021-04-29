3c617381215322f51705ca4c20abf1786e71eb63
C9-LinkRs <linkrsfan@gmail.com>
2021-04-29 19:17:04 +0200

    Project done

--- functions/users/index.js
+++ functions/users/index.js
@@ -1,6 +1,7 @@
 const functions = require("firebase-functions");
 const firebaseApp = require("../db.js");
 const express = require('express');
+const md5 = require("md5");
 const cors = require('cors')({origin: true})
 const app = express();
 
@@ -9,29 +10,45 @@ app.use(cors);
 const db = firebaseApp.admin.firestore();
 
 app.post("/", async (req, res) => {
-  const user = req.body;
+  let user = req.body;
+  user.password = md5(user.password);
+  
   await db.collection("users").add(user);
   res.status(201).json({ "message": "user added" });
 });
 
 app.get("/", async (req, res) => {
-  const snapshot = await db.collection("users").get();
-
   let users = [];
-  snapshot.forEach(doc => {
+  const snapshot = await db.collection("users").get();
+  
+  for(const doc of snapshot.docs) {
     let id = doc.id;
     let data = doc.data();
-    users.push({id, ...data});
-  });
+    let points = [];
 
-  res.status(200).json(JSON.stringify(users));
+    let pointsCollection = await db.collection("users").doc(doc.id).collection("points").get();
+    for(const subDoc of pointsCollection.docs) {
+      points.push({ "id": subDoc.id, "data": subDoc.data() });
+    }
+
+    users.push({id, ...data, points});
+  }
+
+  res.status(200).json(users);
 });
 
 app.get("/:id", async (req, res) => {
+  let points = [];
   const userId = req.params.id;
   const user = await db.collection("users").doc(userId).get();
+  const data = user.data();
 
-  res.status(200).json(JSON.stringify(user));
+  let pointsCollection = await db.collection("users").doc(userId).collection("points").get();
+  for(const subDoc of pointsCollection.docs) {
+    points.push({ "id": subDoc.id, "data": subDoc.data() });
+  }
+
+  res.status(200).json({ "id": user.id,  ...data, points});
 });
 
 app.put("/:id", async (req, res) => {
