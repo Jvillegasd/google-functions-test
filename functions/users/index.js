eb206fdc2451881e6324f7532cc3aa68e285dc35
C9-LinkRs <linkrsfan@gmail.com>
2021-04-29 09:26:58 +0200

    Users crud started

--- functions/users/index.js
+++ functions/users/index.js
@@ -1,12 +1,34 @@
 const functions = require("firebase-functions");
-
 const admin = require('firebase-admin');
-admin.initializeApp();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const serviceAccount  = require("../key.json");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+app.use(cors({ origin: true }));
+
+const db = admin.firestore();
+
+app.post("/", async (req, res) => {
+  const user = req.body;
+  await db.collection("users").add(user);
+  res.status(201).json({ "message": "user added" });
+});
+
+app.get("/", async (req, res) => {
+  const snapshot = await db.collection("users").get();
+
+  let users = [];
+  snapshot.forEach(doc => {
+    let id = doc.id;
+    let data = doc.data();
+    users.push({id, ...data});
+  });
+
+  res.status(200).json(JSON.stringify(users));
+});
+
+exports.user = functions.https.onRequest(app);
